f5fc4e8b80e3268167a5d22cb47026621f39da9a
unknown <naumanrao6869@outlook.com>
2026-06-01 16:50:46 +0500

    site

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,5 +4,17 @@ export default function sitemap() {
             url: "https://sinahmuzaffargarh.vercel.app",
             lastModified: new Date(),
         },
+        {
+            url: "https://sinahmuzaffargarh.vercel.app/about",
+            lastModified: new Date("2026-01-01"),
+        },
+        {
+            url: "https://sinahmuzaffargarh.vercel.app/programs",
+            lastModified: new Date("2026-01-01"),
+        },
+        {
+            url: "https://sinahmuzaffargarh.vercel.app/contact",
+            lastModified: new Date("2026-01-01"),
+        },
     ];
 }
